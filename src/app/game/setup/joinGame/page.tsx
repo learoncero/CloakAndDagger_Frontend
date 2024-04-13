8cec4105707c8186f5d8e73a6c34fcc5c2e82f89
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-04-13 21:03:15 +0200

    added game over feedback for crewmate and changed BackLink.tsx

--- src/app/game/setup/joinGame/page.tsx
+++ src/app/game/setup/joinGame/page.tsx
@@ -44,7 +44,7 @@ export default function JoinGame() {
   return (
     <div className="bg-black flex justify-center items-center h-screen">
       <div className="max-w-md text-white p-8 rounded-lg border-white border">
-        <BackLink href={"/game/setup"} />
+        <BackLink href={"/game/setup"}>Back</BackLink>
         <JoinGameForm stompClient={stompClient} />
       </div>
     </div>
