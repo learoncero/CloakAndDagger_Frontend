658a89a5c25b5032539e8a7d36a660b99c93ccb9
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-04-14 13:52:50 +0200

    added toast for impostor after kill and implemented 20 sec cooldown kill button

--- src/app/game/[gameCode]/play/ImpostorView.tsx
+++ src/app/game/[gameCode]/play/ImpostorView.tsx
@@ -1,12 +1,36 @@
 import { Sabotage } from "@/app/types";
-import RoleInformation from "./RoleInformation";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
 import SabotageList from "./SabotageList";
+import RoleInformation from "./RoleInformation";
+import KillButton from "./KillButton";
 
 type Props = {
   sabotages: Sabotage[];
 };
 
 export default function ImpostorView({ sabotages }: Props) {
+  const [disabled, setDisabled] = useState(false);
+
+  function handleKill() {
+    // Show toast notification for the kill
+    toast("You killed a crewmate!", {
+      position: "bottom-right",
+      style: {
+        border: "2px solid black", // Red border
+        padding: "16px",
+        color: "black", // Text color
+        backgroundColor: "#eF4444", // Red background
+      },
+      icon: "🔪",
+    });
+
+    setDisabled(true);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 20000);
+  }
+
   return (
     <div className="flex justify-between items-start p-4">
       <div className="flex-none">
@@ -22,6 +46,10 @@ export default function ImpostorView({ sabotages }: Props) {
       <div className="flex-none">
         <p>Map Button Component Goes Here</p>
       </div>
+      <div className="absolute bottom-4 right-4">
+        <KillButton handleKill={handleKill} disabled={disabled} />
+      </div>
+      <Toaster />
     </div>
   );
 }
