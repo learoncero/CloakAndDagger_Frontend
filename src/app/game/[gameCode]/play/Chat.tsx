5cadd9f22f0b9d483af3e8685234bf9950603fe4
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-04-25 08:53:46 +0200

    small bug fix - added z index to chat component and modal

--- src/app/game/[gameCode]/play/Chat.tsx
+++ src/app/game/[gameCode]/play/Chat.tsx
@@ -42,7 +42,7 @@ export default function Chat({ onClose, gameCode, players }: Props) {
   }, [stompClient]);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-10">
       <div className="max-w-lg w-full p-10 text-white border border-white rounded-lg relative">
         <button
           className="absolute top-0 right-0 text-white text-lg p-2"
