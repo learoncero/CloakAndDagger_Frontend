a4f26ac428647e98bc938c8a15662b204943d6f2
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-09 10:09:37 +0200

    added voting and results popup components to chat

--- src/app/game/[gameCode]/play/Chat.tsx
+++ src/app/game/[gameCode]/play/Chat.tsx
@@ -6,19 +6,24 @@ import ChatBubble from "./ChatBubble";
 import { endChat } from "./actions";
 import ChatMessageInputField from "./ChatMessageInputField";
 import ChatSendButton from "./ChatSendButton";
+import Voting from "./Voting";
+import VotingResultsPopup from "./VotingResultsPopup";
 
 type Props = {
   onClose: (value: boolean) => void;
   gameCode: string;
   currentPlayer: Player;
+  activePlayers: Player[];
 };
 
-export default function Chat({ onClose, gameCode, currentPlayer }: Props) {
+export default function Chat({ onClose, gameCode, currentPlayer, activePlayers }: Props) {
   const [stompClient, setStompClient] = useState<any>(null);
   const [chat, setChat] = useState<ChatType>({} as ChatType);
   const [message, setMessage] = useState("");
-  const [remainingTime, setRemainingTime] = useState<number>(30);
+  const [remainingTime, setRemainingTime] = useState<number>(300000);
   const chatContainerRef = useRef<HTMLDivElement>(null);
+  const [showVotingResults, setShowVotingResults] = useState(false);
+  const [votedPlayer, setVotedPlayer] = useState<Player | undefined>(undefined);
 
   useEffect(() => {
     if (!stompClient) {
@@ -79,12 +84,22 @@ export default function Chat({ onClose, gameCode, currentPlayer }: Props) {
   }, [chat]);
 
   async function handleEndChat() {
+    setShowVotingResults(true); //todo test this
     await endChat(gameCode as string);
   }
 
   function updateMessage(message: string) {
     setMessage(message);
   }
+
+  function handleVoting(playerId : number) {
+    setVotedPlayer(activePlayers.find(player => player.id === playerId));
+    setShowVotingResults(true)
+  }
+
+    function onCloseResultsPopup() {
+        setShowVotingResults(false)
+    }
 
   async function onMessageSend() {
     const chatMessage = {
@@ -94,9 +109,9 @@ export default function Chat({ onClose, gameCode, currentPlayer }: Props) {
     };
     if (stompClient && message.trim() !== "") {
       stompClient.send(
-        "/app/chat/sendMessage",
-        {},
-        JSON.stringify(chatMessage)
+          "/app/chat/sendMessage",
+          {},
+          JSON.stringify(chatMessage)
       );
     }
     setMessage("");
@@ -105,32 +120,38 @@ export default function Chat({ onClose, gameCode, currentPlayer }: Props) {
   console.log("chat", chat);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-10">
-      <div className="max-w-3xl w-full px-10 text-white border border-white rounded-lg relative min-h-96">
-        <h2 className="text-3xl font-bold mb-4 text-white border-b pt-10 pb-5">
-          Chat - Remaining Time: {remainingTime} seconds
-        </h2>
-        <div
-          ref={chatContainerRef}
-          className="h-96 rounded-lg overflow-y-scroll pb-5 scrollbar-hide scroll-smooth"
-        >
-          {chat?.chatMessages?.map((chatMessage, index) => (
-            <ChatBubble
-              key={index}
-              message={chatMessage.message}
-              sender={chatMessage.sender}
-              currentPlayerName={currentPlayer.username}
+    <div>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-10">
+        <div className="max-w-3xl w-full px-10 text-white border border-white rounded-lg relative min-h-96">
+          <h2 className="text-3xl font-bold mb-4 text-white border-b pt-10 pb-5">
+            Chat - Remaining Time: {remainingTime} seconds
+          </h2>
+          <div
+            ref={chatContainerRef}
+            className="h-96 rounded-lg overflow-y-scroll pb-5 scrollbar-hide scroll-smooth"
+          >
+            {chat?.chatMessages?.map((chatMessage, index) => (
+              <ChatBubble
+                key={index}
+                message={chatMessage.message}
+                sender={chatMessage.sender}
+                currentPlayerName={currentPlayer.username}
+              />
+            ))}
+          </div>
+          <div className="flex items-center justify-between px-5 py-3 border-t border-white gap-5">
+            <ChatMessageInputField
+              updateMessage={updateMessage}
+              message={message}
+              onMessageSend={onMessageSend}
             />
-          ))}
+            <ChatSendButton onMessageSend={onMessageSend} />
+          </div>
         </div>
-        <div className="flex items-center justify-between px-5 py-3 border-t border-white gap-5">
-          <ChatMessageInputField
-            updateMessage={updateMessage}
-            message={message}
-            onMessageSend={onMessageSend}
-          />
-          <ChatSendButton onMessageSend={onMessageSend} />
-        </div>
+        {showVotingResults &&
+            <VotingResultsPopup onCloseResultsPopup={onCloseResultsPopup} votedPlayer={votedPlayer}/>
+        }
+        <Voting activePlayers={activePlayers} handleVoting={handleVoting}/>
       </div>
     </div>
   );
