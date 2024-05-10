6aeb33bf39a5358691ee5f6f31d1dd3476d20c49
Lea Roncero <lea.roncero@students.fhv.at>
2024-05-10 18:37:11 +0200

    added task count to task list

--- src/app/game/[gameCode]/play/TaskList.tsx
+++ src/app/game/[gameCode]/play/TaskList.tsx
@@ -14,7 +14,9 @@ export default function TaskList({ tasks }: Props) {
 
   return (
     <div className="bg-black text-white border border-gray-600 shadow-md rounded-lg p-4 font-sans text-sm w-full max-w-lg min-h-64 overflow-y-auto">
-      <h2 className="text-lg font-semibold mb-4">List of Tasks</h2>
+      <h2 className="text-lg font-semibold mb-4">
+        List of Tasks ({completedTasks}/{totalTasks})
+      </h2>
       <ProgressBar progress={progress} />
       <ul>
         {tasks.map((task, index) => (
