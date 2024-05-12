3e72328651d09b6bec6a2fe7d5958f28b57dfbc5
Jan Burtscher <jan.burtscher98@gmail.com>
2024-05-13 00:45:17 +0200

    Fixed Bug

--- src/app/game/setup/joinGame/JoinGameFormInputField.tsx
+++ src/app/game/setup/joinGame/JoinGameFormInputField.tsx
@@ -1,58 +1,60 @@
-import React, {ChangeEvent} from "react";
+import React, { ChangeEvent } from "react";
 
 type Props = {
-  name: string;
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => void;
-  type: string;
-  placeholder: string;
-  required: boolean;
-  maxLength?: number;
-  options?: string[]; // Updated to string array type
+    name: string;
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => void;
+    type: string;
+    placeholder: string;
+    required: boolean;
+    maxLength?: number;
+    options?: string[];
 };
 
 export default function JoinGameFormInputField({
-                                                 name,
-                                                 value,
-                                                 onChange,
-                                                 type,
-                                                 placeholder,
-                                                 required,
-                                                 maxLength,
-                                                 options = [],
+                                                   name,
+                                                   value,
+                                                   onChange,
+                                                   type,
+                                                   placeholder,
+                                                   required,
+                                                   maxLength,
+                                                   options = [],
                                                }: Props) {
-  if (type === "select") {
-    return (
-        <div className="mb-4" >
-          <select
-              className="font-sans w-full bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center focus:bg-black"
-              name={name}
-              value={value}
-              onChange={onChange}
-              required={required}
-          >
-            {options.map((option) => (
-                <option key={option} value={option} className={"text-lg"}>
-                  {option}
-                </option>
-            ))}
-          </select>
-        </div>
-    );
-  } else {
-    return (
-        <div className="mb-4">
-          <input
-              className="w-full bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
-              type={type}
-              placeholder={placeholder}
-              name={name}
-              value={value}
-              onChange={onChange}
-              required={required}
-              maxLength={maxLength}
-          />
-        </div>
-    );
-  }
+    if (type === "select") {
+        return (
+            <div className="mb-4">
+                <select
+                    className="font-sans w-full bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center focus:bg-black"
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    required={required}
+                >
+
+                    {!value && <option value="" disabled>{placeholder}</option>}
+                    {options.map((option) => (
+                        <option key={option} value={option} className="text-lg">
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        );
+    } else {
+        return (
+            <div className="mb-4">
+                <input
+                    className="w-full bg-transparent border border-white text-white font-bold py-3 rounded-lg text-xl text-center"
+                    type={type}
+                    placeholder={placeholder}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    required={required}
+                    maxLength={maxLength}
+                />
+            </div>
+        );
+    }
 }
