ed2d3008542e4a862507a636e6f12402a91c247f
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-21 23:42:00 +0200

    new component WhoVotedWhoList.tsx

---
+++ src/app/game/[gameCode]/play/WhoVotedWhoList.tsx
@@ -0,0 +1,37 @@
+import {Player, VoteEvent} from "@/app/types";
+
+type WhoVotedWhoProps = {
+    players: Player[];
+  voteEvents: VoteEvent[];
+};
+
+
+export default function WhoVotedWhoList({ players, voteEvents}: WhoVotedWhoProps) {
+
+    type VotesDict = {
+        [key: string]: string[];
+    };
+    //creates a Dictionary of type VotesDict, which groups players by who they voted for
+    const votesDict: VotesDict = voteEvents.reduce((acc: VotesDict, vote: VoteEvent) => {
+        const votedForUsername = players.find(player => player.id === vote.votedForPlayer)?.username;
+        const votedByUsername = players.find(player => player.id === vote.votedBy)?.username;
+
+        if (votedForUsername && votedByUsername) {
+            if (!acc[votedForUsername]) {
+                acc[votedForUsername] = [];
+            }
+            acc[votedForUsername].push(votedByUsername);
+        }
+        return acc;
+    }, {} as VotesDict);
+
+    return (
+        <div className={"mt-1"}>
+            {Object.keys(votesDict).map((votedForUsername, index) => (
+                <div key={index}>
+                    <strong>{votedForUsername}:</strong> {votesDict[votedForUsername].join(', ')}
+                </div>
+            ))}
+        </div>
+    );
+}
