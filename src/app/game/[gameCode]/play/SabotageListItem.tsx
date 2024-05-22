3aa4c541c74944bd993b2dc97ccec8709877fda4
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-22 09:12:44 +0200

    sabotages now dont get deleted

--- src/app/game/[gameCode]/play/SabotageListItem.tsx
+++ src/app/game/[gameCode]/play/SabotageListItem.tsx
@@ -2,17 +2,17 @@ import { Sabotage } from "@/app/types";
 
 type Props = {
   sabotage: Sabotage;
-  onComplete(): void;
+  onActivate(): void;
 };
 
-export default function SabotageListItem({ sabotage, onComplete }: Props) {
+export default function SabotageListItem({ sabotage, onActivate }: Props) {
   return (
     <li className="bg-gray-700 bg-opacity-70 rounded-md p-2 mb-2">
       <span className="block font-semibold">{sabotage.title}</span>
       <p className="text-xs">{sabotage.description}</p>
       <button
         className="text-xs text-blue-500 hover:underline mt-1"
-        onClick={onComplete}
+        onClick={onActivate}
       >
         Complete
       </button>
