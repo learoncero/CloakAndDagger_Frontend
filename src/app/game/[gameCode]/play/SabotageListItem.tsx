20d6f706a73144bb919cfe6a76a11c8e29369fa6
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-22 10:33:46 +0200

    updated font size

--- src/app/game/[gameCode]/play/SabotageListItem.tsx
+++ src/app/game/[gameCode]/play/SabotageListItem.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function SabotageListItem({ sabotage, onComplete }: Props) {
   return (
     <li className="bg-gray-700 bg-opacity-70 rounded-md p-2 mb-2">
-      <p className="block font-semibold text-lg">{sabotage.title}</p>
+      <p className="block font-semibold text-base">{sabotage.title}</p>
       <p className="text-s mb-3">{sabotage.description}</p>
         <button
             className={`px-3 py-1 rounded-md text-black font-semibold bg-cyan-500 cursor-default hover:bg-cyan-600 hover:cursor-pointer`}
