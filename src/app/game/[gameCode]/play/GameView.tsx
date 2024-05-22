7535843290aea652c6572ba68a5caf3ac50994b6
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-22 20:22:26 +0200

    added gap between sabotage / task list and map

--- src/app/game/[gameCode]/play/GameView.tsx
+++ src/app/game/[gameCode]/play/GameView.tsx
@@ -350,7 +350,7 @@ export default function GameView({
               <TaskList tasks={game.tasks} />
             )}
             <div
-              className="flex flex-col items-center p-2 bg-white rounded-lg shadow-md justify-center flex-warp"
+              className="ml-8 flex flex-col items-center p-2 bg-white rounded-lg shadow-md justify-center flex-warp"
               onClick={(e) => e.stopPropagation()}
             >
               <MiniMap
