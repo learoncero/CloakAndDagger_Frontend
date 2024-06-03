7f8f07c0242b9922814a405f060fe9a151563819
Sarah Neville <sarah.neville@students.fhv.at>
2024-06-03 13:13:02 +0200

    new VentIconDisplay.tsx file

---
+++ src/app/game/[gameCode]/play/VentIconDisplay.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+type VentProps = {
+    isVentInteractable: boolean;
+    isVisible: boolean;
+}
+
+export default function VentIconDisplay ({
+    isVentInteractable,
+    isVisible,
+ }: VentProps) {
+
+    return (
+        <div className={`flex place-content-center w-full h-full z-10 ${!isVisible ? "opacity-50" : ""}`}>
+            <Image
+                src={"/vent.png"}
+                alt={"Vent"}
+                width={100}
+                height={100}
+                className={`object-contain p-[10%]`}
+            />
+            {isVentInteractable && (
+                <div className="absolute top-1 right-2 text-black font-bold bg-white px-1 rounded-full">
+                    V
+                </div>
+            )}
+        </div>
+    )
+}
