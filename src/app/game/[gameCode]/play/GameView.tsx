a11aa2d70a73bd512102191a30e4778da1b3b30a
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-06-11 20:40:36 +0200

    minor bug fix

--- src/app/game/[gameCode]/play/GameView.tsx
+++ src/app/game/[gameCode]/play/GameView.tsx
@@ -27,9 +27,9 @@ type Props = {
   getSabotagePosition: (sabotageId: number) => void;
   handleCancelSabotage: () => void;
   killPlayer: (
-      gameCode: string,
-      playerId: number,
-      nearbyTaskForKill: number
+    gameCode: string,
+    playerId: number,
+    nearbyTaskForKill: number
   ) => void;
   reportBody: (gameCode: string, playerId: number) => void;
   handleTaskCompleted: (taskId: number) => void;
@@ -68,25 +68,25 @@ export default function GameView({
   stompClient,
 }: Props) {
   const isImpostor =
-      currentPlayer?.role == Role.IMPOSTOR ||
-      currentPlayer?.role == Role.IMPOSTOR_GHOST;
+    currentPlayer?.role == Role.IMPOSTOR ||
+    currentPlayer?.role == Role.IMPOSTOR_GHOST;
   const isGhost =
-      currentPlayer?.role == Role.CREWMATE_GHOST ||
-      currentPlayer?.role == Role.IMPOSTOR_GHOST;
+    currentPlayer?.role == Role.CREWMATE_GHOST ||
+    currentPlayer?.role == Role.IMPOSTOR_GHOST;
   const [showMiniMap, setShowMiniMap] = useState(false);
   const [isTimer, setIsTimer] = useState(false);
   const [showManual, setShowManual] = useState(false);
   const [showRockPaperScissor, setShowRockPaperScissor] = useState(false);
 
   const isSabotageActive = (
-      sabotageId: number,
-      position: { x: number; y: number }
+    sabotageId: number,
+    position: { x: number; y: number }
   ) => {
     return game.sabotages.some(
-        (sabotage) =>
-            sabotage.id === sabotageId &&
-            (sabotage.position.x !== position.x ||
-                sabotage.position.y !== position.y)
+      (sabotage) =>
+        sabotage.id === sabotageId &&
+        (sabotage.position.x !== position.x ||
+          sabotage.position.y !== position.y)
     );
   };
 
@@ -114,34 +114,34 @@ export default function GameView({
   }, [showManual]);
 
   const nearbyTasks = useNearbyItems(
-      game.tasks,
-      currentPlayer.playerPosition
+    game.tasks,
+    currentPlayer.playerPosition
   ) as Task[];
 
   const nearbyTasksForKill = useNearbyItems(
-      game.tasks,
-      currentPlayer.playerPosition
+    game.tasks,
+    currentPlayer.playerPosition
   ) as Task[];
 
   const nearbySabotages = useNearbyItems(
-      game.sabotages,
-      currentPlayer.playerPosition
+    game.sabotages,
+    currentPlayer.playerPosition
   ) as Sabotage[];
 
   const nearbyPlayers = useNearbyPlayers(
-      game?.players || [],
-      currentPlayer as Player,
-      [Role.CREWMATE, Role.IMPOSTOR]
+    game?.players || [],
+    currentPlayer as Player,
+    [Role.CREWMATE, Role.IMPOSTOR]
   );
 
   const nearbyDeadBodies = useNearbyDeadBodies(
-      game?.players || [],
-      currentPlayer as Player
+    game?.players || [],
+    currentPlayer as Player
   );
 
   const nearbyWalls = useNearbyWall(
-      game.sabotages,
-      currentPlayer.playerPosition
+    game.sabotages,
+    currentPlayer.playerPosition
   );
 
   const handleToggleTaskPopup = useCallback(async () => {
@@ -154,9 +154,9 @@ export default function GameView({
         handleShowTaskPopup(false);
         await setActiveStatus();
         await TaskService.cancelTask(
-            nearbyTasks[0].taskId,
-            nearbyTasks[0].miniGameId,
-            game.gameCode
+          nearbyTasks[0].taskId,
+          nearbyTasks[0].miniGameId,
+          game.gameCode
         );
       } else {
         handleShowTaskPopup(true);
@@ -171,9 +171,12 @@ export default function GameView({
         handleKill();
       }
       if (
-          isSabotageActive(3, { x: -1, y: -1 }) &&
-          (event.key === "m" || event.key === "M" || event.key === "q" || event.key === "Q") &&
-          !isImpostor
+        isSabotageActive(3, { x: -1, y: -1 }) &&
+        (event.key === "m" ||
+          event.key === "M" ||
+          event.key === "q" ||
+          event.key === "Q") &&
+        !isImpostor
       ) {
         setShowMiniMap(false);
         toast("Minimap is disabled due to sabotage!", {
@@ -188,21 +191,22 @@ export default function GameView({
         });
       } else if (
         (event.key === "m" ||
-        event.key === "M" ||
-        event.key === "q" ||
-        event.key === "Q") &&
-        !showChat) {
-          setShowMiniMap((prev) => !prev);
+          event.key === "M" ||
+          event.key === "q" ||
+          event.key === "Q") &&
+        !showChat
+      ) {
+        setShowMiniMap((prev) => !prev);
       }
       if (event.code === "KeyR") {
         handleReportBody();
       } else if (
-          (event.key === "F" || event.key === "f") &&
-          !showChat &&
-          !showMiniMap &&
-          !showEmergencyMeeting &&
-          !isEmergencyMeetingTimeout &&
-          !isGhost
+        (event.key === "F" || event.key === "f") &&
+        !showChat &&
+        !showMiniMap &&
+        !showEmergencyMeeting &&
+        !isEmergencyMeetingTimeout &&
+        !isGhost
       ) {
         callEmergencyMeeting(game.gameCode);
       } else if (event.code === "KeyG" && nearbyWalls.length > 0) {
@@ -223,22 +227,24 @@ export default function GameView({
 
   useEffect(() => {
     const handleKeyPress = async (event: KeyboardEvent) => {
-      if ((event.key === "e" || event.key === "E") &&
-          !showChat &&
-          !showMiniMap &&
-          !showEmergencyMeeting) {
+      if (
+        (event.key === "e" || event.key === "E") &&
+        !showChat &&
+        !showMiniMap &&
+        !showEmergencyMeeting
+      ) {
         if (
-            nearbyTasks.length === 0 ||
-            (currentPlayer?.role !== Role.CREWMATE &&
-                currentPlayer?.role !== Role.CREWMATE_GHOST) ||
-            nearbyTasks[0]?.completed
+          nearbyTasks.length === 0 ||
+          (currentPlayer?.role !== Role.CREWMATE &&
+            currentPlayer?.role !== Role.CREWMATE_GHOST) ||
+          nearbyTasks[0]?.completed
         ) {
           return;
         }
 
         const status = await TaskService.getActiveStatus(
-            nearbyTasks[0].taskId,
-            game.gameCode
+          nearbyTasks[0].taskId,
+          game.gameCode
         );
 
         if (status.data === true && !showTaskPopup) {
@@ -256,9 +262,9 @@ export default function GameView({
           return;
         } else if (status.data === false && !showTaskPopup) {
           const response = await TaskService.startTask(
-              nearbyTasks[0].taskId,
-              nearbyTasks[0].miniGameId,
-              game.gameCode
+            nearbyTasks[0].taskId,
+            nearbyTasks[0].miniGameId,
+            game.gameCode
           );
           if (response.status === 200) {
             await handleToggleTaskPopup();
@@ -267,7 +273,7 @@ export default function GameView({
           await handleToggleTaskPopup();
         }
       }
-      if(event.code === "KeyV" && isImpostor && !showMiniMap && !showMiniMap) {
+      if (event.code === "KeyV" && isImpostor && !showMiniMap && !showMiniMap) {
         handleVentUsage(game.gameCode, currentPlayer.id);
       }
     };
@@ -311,9 +317,9 @@ export default function GameView({
     if (!isTimer && nearbyPlayers.length > 0) {
       const playerToKillId = nearbyPlayers[0].id;
       killPlayer(
-          game?.gameCode as string,
-          playerToKillId,
-          nearbyTasksForKill[0]?.taskId
+        game?.gameCode as string,
+        playerToKillId,
+        nearbyTasksForKill[0]?.taskId
       );
 
       toast("You killed a crewmate!", {
@@ -344,153 +350,154 @@ export default function GameView({
   }
 
   return (
-      <div>
-        {showManual && (
-            <Manual role={currentPlayer.role} onClose={toggleManualVisibility} />
-        )}
-        {showBodyReported && (
-            <InformationPopUp
-                imageSrc={"/bodyReported.png"}
-                heading={"Body Reported!"}
-                text={"Oh no! Looks like someone is taking a long nap!"}
-                onDismiss={() => handleShowBodyReported(false)}
-            />
-        )}
-        {showEmergencyMeeting && (
-            <InformationPopUp
-                imageSrc={"/emergencyMeeting.png"}
-                heading={"Emergency Meeting!"}
-                text={
-                  "Attention crew! An emergency meeting has been initiated. Share your suspicions and vote to find the impostor!"
-                }
-                onDismiss={() => handleEmergencyMeeting(false)}
-            />
-        )}
-        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start p-5 lg:p-10">
-          <div className="flex-none w-1/4">
-            <RoleInformation role={currentPlayer.role} />
-            {isImpostor ? (
-                <SabotageList
-                    sabotages={game.sabotages}
-                    getSabotagePosition={getSabotagePosition}
-                    showMiniMap={showMiniMap}
-                />
-            ) : (
-                <TaskList tasks={game.tasks} />
-            )}
-          </div>
-          <div className="flex-grow flex justify-center">
-            {map ? (
-                <MapDisplay
-                    map={map}
-                    playerList={game.players}
-                    currentPlayer={currentPlayer}
-                    tasks={game.tasks}
-                    sabotages={game.sabotages ?? []}
-                    nearbyTask={nearbyTasks[0]}
-                    isEmergencyMeetingTimeout={isEmergencyMeetingTimeout}
-                />
-            ) : (
-                <div>Loading map...</div>
-            )}
-          </div>
-
-          <div className="flex-none w-1/4">
-            <div className="mb-7">
-              <div className="flex gap-10">
-                <ToggleButton
-                    onClick={handleToggleMiniMap}
-                    label="Show Minimap"
-                />
-                <ToggleButton
-                    onClick={toggleManualVisibility}
-                    label="Show Manual"
-                />
-              </div>
-              <PlayerList playerId={currentPlayer.id} playerList={game.players} />
-              {isImpostor && <CrewmateCounter playerList={game.players} />}
-            </div>
-
-            <div className="flex gap-5 justify-center">
-              {isImpostor && (
-                  <ActionButton
-                      onClick={handleKill}
-                      buttonclickable={
-                          nearbyPlayers.length > 0 &&
-                          !isTimer &&
-                          currentPlayer.role === Role.IMPOSTOR
-                      }
-                      colorActive="bg-red-600"
-                  >
-                    {isTimer ? "⏳ Kill on cooldown" : "🔪 Kill"}
-                  </ActionButton>
-              )}
-              <ActionButton
-                  onClick={() => handleReportBody()}
-                  buttonclickable={
-                      nearbyDeadBodies.length > 0 &&
-                      !game.reportedBodies.includes(nearbyDeadBodies[0].id)
-                  }
-                  colorActive="bg-cyan-600"
-              >
-                📢 Report Body
-              </ActionButton>
-            </div>
-          </div>
-          <Toaster />
-          {showMiniMap && (
-              <div
-                  className="fixed flex justify-center items-center bg-black bg-opacity-75 z-1000 overflow-auto size-full"
-                  onClick={() => setShowMiniMap(false)}
-              >
-                {isImpostor ? (
-                    <SabotageList
-                        sabotages={game.sabotages}
-                        getSabotagePosition={getSabotagePosition}
-                        showMiniMap={showMiniMap}
-                    />
-                ) : (
-                    <TaskList tasks={game.tasks} />
-                )}
-                <div
-                    className="ml-8 flex items-center p-2 bg-white rounded-lg shadow-md justify-center flex-warp"
-                    onClick={(e) => e.stopPropagation()}
-                >
-                  <MiniMap
-                      map={map}
-                      playerList={game.players}
-                      currentPlayer={currentPlayer}
-                      closeMiniMap={() => setShowMiniMap(false)}
-                      tasks={game.tasks}
-                      sabotages={game.sabotages}
-                  />
-                </div>
-              </div>
-          )}
-
+    <div>
+      {showManual && (
+        <Manual role={currentPlayer.role} onClose={toggleManualVisibility} />
+      )}
+      {showBodyReported && (
+        <InformationPopUp
+          imageSrc={"/bodyReported.png"}
+          heading={"Body Reported!"}
+          text={"Oh no! Looks like someone is taking a long nap!"}
+          onDismiss={() => handleShowBodyReported(false)}
+        />
+      )}
+      {showEmergencyMeeting && (
+        <InformationPopUp
+          imageSrc={"/emergencyMeeting.png"}
+          heading={"Emergency Meeting!"}
+          text={[
+            "Attention crew! An emergency meeting has been initiated.",
+            "Share your suspicions and vote to find the impostor!",
+          ]}
+          onDismiss={() => handleEmergencyMeeting(false)}
+        />
+      )}
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start p-5 lg:p-10">
+        <div className="flex-none w-1/4">
+          <RoleInformation role={currentPlayer.role} />
           {isImpostor ? (
-              <Toaster />
-          ) : showTaskPopup ? (
-              <TaskGateway
-                  miniGameId={nearbyTasks[0].miniGameId}
-                  taskId={nearbyTasks[0].taskId}
-                  gameCode={game.gameCode}
-                  handleTaskCompleted={() =>
-                      handleTaskCompleted(nearbyTasks[0].taskId)
-                  }
-              />
+            <SabotageList
+              sabotages={game.sabotages}
+              getSabotagePosition={getSabotagePosition}
+              showMiniMap={showMiniMap}
+            />
           ) : (
-              ""
-          )}
-          {showRockPaperScissor && (
-              <RockPaperScissor
-                  stompClient={stompClient}
-                  gameCode={game.gameCode}
-                  onConfirm={handleChoice}
-                  onCancel={() => setShowRockPaperScissor(false)}
-              />
+            <TaskList tasks={game.tasks} />
           )}
         </div>
+        <div className="flex-grow flex justify-center">
+          {map ? (
+            <MapDisplay
+              map={map}
+              playerList={game.players}
+              currentPlayer={currentPlayer}
+              tasks={game.tasks}
+              sabotages={game.sabotages ?? []}
+              nearbyTask={nearbyTasks[0]}
+              isEmergencyMeetingTimeout={isEmergencyMeetingTimeout}
+            />
+          ) : (
+            <div>Loading map...</div>
+          )}
+        </div>
+
+        <div className="flex-none w-1/4">
+          <div className="mb-7">
+            <div className="flex gap-10">
+              <ToggleButton
+                onClick={handleToggleMiniMap}
+                label="Show Minimap"
+              />
+              <ToggleButton
+                onClick={toggleManualVisibility}
+                label="Show Manual"
+              />
+            </div>
+            <PlayerList playerId={currentPlayer.id} playerList={game.players} />
+            {isImpostor && <CrewmateCounter playerList={game.players} />}
+          </div>
+
+          <div className="flex gap-5 justify-center">
+            {isImpostor && (
+              <ActionButton
+                onClick={handleKill}
+                buttonclickable={
+                  nearbyPlayers.length > 0 &&
+                  !isTimer &&
+                  currentPlayer.role === Role.IMPOSTOR
+                }
+                colorActive="bg-red-600"
+              >
+                {isTimer ? "⏳ Kill on cooldown" : "🔪 Kill"}
+              </ActionButton>
+            )}
+            <ActionButton
+              onClick={() => handleReportBody()}
+              buttonclickable={
+                nearbyDeadBodies.length > 0 &&
+                !game.reportedBodies.includes(nearbyDeadBodies[0].id)
+              }
+              colorActive="bg-cyan-600"
+            >
+              📢 Report Body
+            </ActionButton>
+          </div>
+        </div>
+        <Toaster />
+        {showMiniMap && (
+          <div
+            className="fixed flex justify-center items-center bg-black bg-opacity-75 z-1000 overflow-auto size-full"
+            onClick={() => setShowMiniMap(false)}
+          >
+            {isImpostor ? (
+              <SabotageList
+                sabotages={game.sabotages}
+                getSabotagePosition={getSabotagePosition}
+                showMiniMap={showMiniMap}
+              />
+            ) : (
+              <TaskList tasks={game.tasks} />
+            )}
+            <div
+              className="ml-8 flex items-center p-2 bg-white rounded-lg shadow-md justify-center flex-warp"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <MiniMap
+                map={map}
+                playerList={game.players}
+                currentPlayer={currentPlayer}
+                closeMiniMap={() => setShowMiniMap(false)}
+                tasks={game.tasks}
+                sabotages={game.sabotages}
+              />
+            </div>
+          </div>
+        )}
+
+        {isImpostor ? (
+          <Toaster />
+        ) : showTaskPopup ? (
+          <TaskGateway
+            miniGameId={nearbyTasks[0].miniGameId}
+            taskId={nearbyTasks[0].taskId}
+            gameCode={game.gameCode}
+            handleTaskCompleted={() =>
+              handleTaskCompleted(nearbyTasks[0].taskId)
+            }
+          />
+        ) : (
+          ""
+        )}
+        {showRockPaperScissor && (
+          <RockPaperScissor
+            stompClient={stompClient}
+            gameCode={game.gameCode}
+            onConfirm={handleChoice}
+            onCancel={() => setShowRockPaperScissor(false)}
+          />
+        )}
       </div>
+    </div>
   );
 }
