8c67dbd1bdd3c388c3ee003f39751866f70cd4ef
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-06-12 15:09:35 +0200

    adjusted impostor counter text

--- src/app/game/[gameCode]/play/ImpostorCounter.tsx
+++ src/app/game/[gameCode]/play/ImpostorCounter.tsx
@@ -11,7 +11,10 @@ export default function ImpostorCounter({ playerList }: Props) {
 
   return (
     <div className="pt-4 pb-1 font-semibold font-sans text-lg">
-      <h2>Identify the Impostors: {impostors.length} remaining!</h2>
+      <h2>
+        Beware! {impostors.length} Impostor{impostors.length !== 1 && "s"} among
+        us!
+      </h2>
     </div>
   );
 }
